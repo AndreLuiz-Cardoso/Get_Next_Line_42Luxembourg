2236ecae09176265b1438f055b4af237ca0d9807
Andre Luiz Cardoso <andre.lc93@hotmail.com>
2024-04-30 15:28:39 +0200

    first

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancardos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 14:56:13 by ancardos          #+#    #+#             */
+/*   Updated: 2024/03/21 14:56:15 by ancardos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char	*s);
+size_t	ft_strlcpy(char	*dest, const char	*src, size_t	size);
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char	*s1, char const	*s2);
+
+#endif
